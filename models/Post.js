7c70e1b1f4ac0820d1674868cbfa65b608f53481
dziubenzo <dziubenzo@gmail.com>
2024-03-21 17:37:18 +0100

    Create Post schema/model

---
+++ models/Post.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 160,
+    unique: true,
+  },
+  content: {
+    type: String,
+    required: true,
+    minLength: 3,
+  },
+  create_date: {
+    type: Date,
+    default: Date.now(),
+  },
+  update_date: {
+    type: Date,
+    default: Date.now(),
+  },
+  published: {
+    type: Boolean,
+    default: false,
+  },
+  likes: {
+    type: Number,
+    min: 0,
+    default: 0,
+  },
+  slug: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 200,
+  },
+});
+
+module.exports = mongoose.model('Post', PostSchema);

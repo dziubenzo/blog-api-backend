0e25d0873c247f667f14ef269323ce1d0b6d9a5d
dziubenzo <dziubenzo@gmail.com>
2024-03-23 16:41:43 +0100

    Add cors() middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 // Authentication imports
 const session = require('express-session');
@@ -31,6 +32,7 @@ async function main() {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 // Routes
 app.use('/', indexRouter);
